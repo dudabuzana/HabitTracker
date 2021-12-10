50cfb9ca291bc1da37ae48d63a76fc30c9e9aaf4
dudabuzana <mdudabuzana16@gmail.com>
2021-12-09 22:01:25 -0300

    Upload Foto
    Desenvolvimento Botão Upload de Foto na Alteração de Perfil

--- app/src/main/java/com/example/habittracker/AlteracaoPerfilActivity.java
+++ app/src/main/java/com/example/habittracker/AlteracaoPerfilActivity.java
@@ -2,19 +2,24 @@ package com.example.habittracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 public class AlteracaoPerfilActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 1888;
     ImageView btnVoltar;
-    Button btnAlterar;
-    EditText edtNome;
-    EditText edtEmail;
-    EditText edtDataNascimento;
+    Button    btnAlterar;
+    Button    btnUploadFoto;
+    EditText  edtNome;
+    EditText  edtEmail;
+    EditText  edtDataNascimento;
 
     Intent i;
 
@@ -34,13 +39,42 @@ public class AlteracaoPerfilActivity extends AppCompatActivity {
             startActivity(i);
         });
 
+        btnUploadFoto.setOnClickListener( v -> {
+            i = new Intent();
+            i.setType("image/*");
+            i.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(i, "Select Picture"), REQUEST_CODE);
+            startActivity(i);
+        });
+
+    }
+
+    //Seleção da Imagem
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            switch (requestCode) {
+                case REQUEST_CODE:
+                    if (resultCode == Activity.RESULT_OK) {
+                        //bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                        //data gives you the image uri. Try to convert that to bitmap
+                        break;
+                    } else if (resultCode == Activity.RESULT_CANCELED) {
+
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void initComponents() {
-        btnVoltar = findViewById(R.id.btnVoltar);
-        btnAlterar = findViewById(R.id.btnAlteracao);
-        edtNome = findViewById(R.id.edtNomeAlteracao);
+        btnVoltar         = findViewById(R.id.btnVoltar);
+        btnAlterar        = findViewById(R.id.btnAlteracao);
+        btnUploadFoto     = findViewById(R.id.btnUploadFoto);
+        edtNome           = findViewById(R.id.edtNomeAlteracao);
         edtDataNascimento = findViewById(R.id.edtDataAlteracao);
-        edtEmail = findViewById(R.id.edtEmailAlteracao);
+        edtEmail          = findViewById(R.id.edtEmailAlteracao);
     }
 }
